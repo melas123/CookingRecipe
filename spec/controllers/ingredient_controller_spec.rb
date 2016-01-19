d29b5fb1afb293b905be695a5f4875156734381d
Mokhles <goalsinhd6@gmail.com>
2016-01-19 16:02:14 +0100

    Refactor the code of ingredient controller and tests
    Details
    ------------
    	* Less logic in controller (skinny controller)
    	* Use FactoryGirl
    	* Use Faker gem
    	* in place of factories ingredient_withname ingredient_withoutname
    	  we have now a factory with name ingredient with two "trait" with_name and without_name.
    	  same logic for the rest of factorie.
    
    	* FIX not doing unnecessary tests from controllers
    	* FIX indent (convert tab to spaces)
    	* Change hash ruby
    	* remove unused variables

--- spec/controllers/ingredient_controller_spec.rb
+++ spec/controllers/ingredient_controller_spec.rb
@@ -6,92 +6,43 @@ RSpec.describe IngredientController, type: :controller do
   describe "GET #new" do
     
     it "add the ingredient" do
-      post :new ,format: :json,name: "Ing"
+      name = Faker::SlackEmoji.food_and_drink
+      post :new ,format: :json,name: name
       expect(response).to have_http_status(:success)
-      expect(Ingredient.find_by_name("Ing")).to_not be_nil
+      expect(Ingredient.find_by_name(name)).to_not be_nil
     end
-
-    it "didn't get no params" do
-      post :new, format: :json
-      expect(response).to have_http_status(500)
-    end
-
-    it "give a dupplicate name" do
-      post :new ,format: :json,name: "Ing"
-      post :new ,format: :json,name: "Ing"
-      expect(response).to have_http_status(500)
-    end
-
   end
 
   describe "GET #index" do
+    let!(:ingredient)  { create(:ingredient,:with_name,name: Faker::SlackEmoji.food_and_drink) }
+    let!(:ingredient2) { create(:ingredient,:with_name,name: "Another name") }
 
     it "returns added ingredients" do
-      i = Ingredient.new(:name => "Ing")
-      i.save
       get :index,format: :json
-      expect(assigns[:ingredients]).to include(i)
+      expect(assigns[:ingredients]).to include(ingredient)
     end
-
-    it "returns all ingredients in json format" do
-      Ingredient.create!(:name => "Ing1")
-      Ingredient.create!(:name => "Ing2")
-      get :index,format: :json
-      parsed_response = JSON.parse(response.body)
-      expect(parsed_response.length).to eq(2)
-    end
-
   end
 
   describe "GET #update" do
+    let!(:ingredient) { create(:ingredient,:with_name) }
 
     it "change the ingredient name" do
-      i = Ingredient.new(:name => "Ing")
-      i.save
-      json = { :format => 'json', :name => "Ing2", :id => i.id }
+      new_name = Faker::SlackEmoji.food_and_drink
+      json = { format: 'json', name: new_name, id: ingredient.id }
       put :update,json
-      expect(Ingredient.find(i.id).name).to eq("Ing2")
-    end
-
-    it "gets no id" do 
-      json = { :format => 'json', :name => "Ing2", :id => nil }
-      put :update,json
-      expect(response).to have_http_status(500)
-    end
-
-    it "gets no name" do 
-      json = { :format => 'json', :name => nil, :id => 1 }
-      put :update,json
-      expect(response).to have_http_status(500)
-    end
-
-    it "no existant id" do
-      #we know that there's no record with an id = 1324
-      json = { :format => 'json', :name => "Ing2", :id => 1324 }
-      put :update,json
-      expect(response).to have_http_status(500)
+      expect(Ingredient.find(ingredient.id).name).to eq(new_name)
     end
   end
 
   describe "GET #delete" do
+    let!(:ingredient) { create(:ingredient,:with_name) }
 
     it "deletes when a record is found" do
-      i = Ingredient.new(:name => "Ing")
-      i.save
-      json = { :format => 'json', :id => i.id }
+      json = { format: 'json', id: ingredient.id }
       delete :delete,json
       expect(response).to have_http_status(:success)
-      expect(Ingredient.exists?(i.id)).to be false
+      expect(Ingredient.exists?(ingredient.id)).to be false
     end
-
-    it "no existant id" do
-      #we know that there's no record with an id = 1324
-      json = { :format => 'json', :id => 1324 }
-      delete :delete,json
-      expect(response).to have_http_status(500)
-    end
-
-
   end
 
 

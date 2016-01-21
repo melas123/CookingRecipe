46a94dc2c2b06ae76cf12a44c197af9225b59140
Mokhles <goalsinhd6@gmail.com>
2016-01-21 09:59:43 +0100

    FEATURE Relationships (user follows others)
    REF #[18](https://trello.com/c/BGwKcQsi)
    
    Details
    ------------------
        add assosiation
        add controller
        add tests
        remove auto-generated files

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :recipes
   has_many :comments
+
   has_many :active_relationships,  class_name:  "Relationship",
                                    foreign_key: "follower_id",
                                    dependent:   :destroy
@@ -14,5 +15,6 @@ class User < ActiveRecord::Base
   has_many :following, through: :active_relationships,  source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
   has_many :rates
+
 
 end

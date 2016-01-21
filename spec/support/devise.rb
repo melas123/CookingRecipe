725077687eb46d81c177d9be134eaea6d258979c
Mokhles <goalsinhd6@gmail.com>
2016-01-21 09:59:43 +0100

    FEATURE Relationships (user follows others)
    REF #[18](https://trello.com/c/BGwKcQsi)
    
    Details
    ------------------
        add assosiation
        add controller
        add tests
        remove auto-generated files

--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -3,7 +3,3 @@ require 'devise'
 RSpec.configure do |config|
   config.include Devise::TestHelpers, :type => :controller
 end
-
-RSpec.configure do |config|
-  config.include Devise::TestHelpers, :type => :controller
-end

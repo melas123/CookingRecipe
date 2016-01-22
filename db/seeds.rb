09e3e09b1d821a9366efa9a2c180d503b339789f
SamiDghim <samidghim@gmail.com>
2016-01-22 12:09:12 +0200

    Add name field to user model
    permitte the name parameter to devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@
 
 puts 'Create users :'
 %w( admin user1 user2 user3 ).each do |name|
-  User.create( email: "#{name}@example.com", password: "#{name}password" )
+  User.create( email: "#{name}@example.com", password: "#{name}password", name: "#{name}" )
 end
 
 puts 'Create a list of ingredients :'
@@ -13,7 +13,12 @@ end
 
 puts 'Create a list of recipes :'
 30.times do |n|
-  Recipe.create( title: "recipe#{n}", description:"description for recipe #{n}", user_id: User.first.id )
+  Recipe.create( title: "recipe#{n}", description:"description for recipe #{n}")
+end
+
+puts 'add all recipes to the first user'
+( Recipe.all ).each do |recipe|
+  ( User.first ).recipes << recipe
 end
 
 puts 'add ingredients to first recipe:'
